d0a28239720c96f2d0cadb3774e259a2997ea450
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-12-30 17:48:32 +0100

    scrape tv and radio broadcasts of ard

--- ardscraper.rb
+++ ardscraper.rb
@@ -1,5 +1,5 @@
 require 'wombat'
-require 'pry'
+require 'csv'
 require 'pp'
 
 
@@ -7,27 +7,50 @@ Wombat.configure do |config|
   config.set_user_agent "Linux Mozilla"
 end
 
-result = Wombat.crawl do
-  base_url "http://www.ardmediathek.de/"
-  path 'tv/sendungen-a-z?buchstabe=Z'
+media = ['radio', 'tv']
+characters = ('A'..'Z').to_a + ['0-9']
+scraped = []
+base_url =  "http://www.ardmediathek.de"
 
-  content 'css=.onlyWithJs .textWrapper', :iterator do
-    title 'css=h4.headline'
-    station 'css=p.subtitle'
-    ardMediathek "xpath=a[contains(@class, 'textLink')]", :follow do
-      description 'css=.onlyWithJs .teasertext'
+media.each do |medium|
+  characters.each do |character|
+    puts "Medium: #{medium} - Character #{character}..."
+    scraped_character = Wombat.crawl do
+      base_url "#{base_url}/"
+      path "#{medium}/sendungen-a-z?buchstabe=#{character}"
+
+      content 'css=.onlyWithJs .textWrapper', :iterator do
+        title 'css=h4.headline'
+        station 'css=p.subtitle'
+        ardMediathek "xpath=a[contains(@class, 'textLink')]", :follow do
+          description 'css=.onlyWithJs .teasertext'
+          imageURL "xpath=//img[contains(@class, 'hideOnNoScript')]/@data-ctrl-image" do |s|
+            "#{base_url}#{s[/'urlScheme':'(.*)##width##'/, 1]}320"
+          end
+        end
+        issues 'css=p.dachzeile' do |s|
+          s[/(\d+) Ausgabe/, 1]
+        end
+        medium medium.to_s
+        character "#{character}"
+        ardMediathekURL "xpath=a[contains(@class, 'textLink')]/@href" do |s|
+          "#{base_url}#{s}"
+        end
+        documentId "xpath=a[contains(@class, 'textLink')]/@href" do |s|
+          s[/documentId=(\d+)/, 1]
+        end
+        bcastId "xpath=a[contains(@class, 'textLink')]/@href" do |s|
+          s[/bcastId=(\d+)/, 1]
+        end
+      end
     end
-    ardMediathekURL "xpath=a[contains(@class, 'textLink')]/@href"
-    documentId "xpath=a[contains(@class, 'textLink')]/@href" do |s|
-      s[/documentId=(\d+)/, 1]
-    end
-    bcastId "xpath=a[contains(@class, 'textLink')]/@href" do |s|
-      s[/bcastId=(\d+)/, 1]
-    end
-    issues 'css=p.dachzeile' do |s|
-      s[/(\d+) Ausgabe/, 1]
-    end
+    scraped.push(*scraped_character['content'])
   end
 end
 
-pp result
+CSV.open("broadcasts.csv", "w", force_quotes: true) do |csv|
+  csv << ['title', 'station', 'description', 'issues', 'medium', 'character', 'url', 'imageURL', 'bcastId', 'documentId']
+  scraped.each do |row|
+    csv << [row['title'], row['station'], row['ardMediathek'][0]['description'], row['issues'], row['medium'], row['character'], row['ardMediathekURL'], row['ardMediathek'][0]['imageURL'], row['bcastId'], row['documentId']]
+  end
+end
